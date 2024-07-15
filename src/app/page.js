b9abcbf2967b49fcef8c140700c25a638defed50
Tatyanac94 <ms.cuttino94@gmail.com>
2024-07-15 03:14:57 -0400

    fixed file path

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import QuoteGenerator from "@/components/quoteGenerator";
+import QuoteGenerator from "../components/quoteGenerator.jsx";
 
 export default function Home() {
   return (
